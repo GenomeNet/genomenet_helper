969d998c5e368ffb72b3097950de3031de2e0d8c
Philipp Muench <philipp.muench@helmholtz-hzi.de>
2023-07-21 13:58:37 +0200

    add wrapper

--- subsample_genomes.py
+++ subsample_genomes.py
@@ -1,6 +1,5 @@
 import os
 import random
-import argparse
 from Bio import SeqIO
 
 def sample_fasta_files(input_dir, output_dir, fragment_length, n_fragments):
@@ -15,7 +14,6 @@ def sample_fasta_files(input_dir, output_dir, fragment_length, n_fragments):
         input_file = os.path.join(input_dir, file)
         output_file = os.path.join(output_dir, file)
 
-        # Combine all sequences in the file into one long sequence
         combined_seq = "".join(str(record.seq) for record in SeqIO.parse(input_file, 'fasta'))
 
         if len(combined_seq) < fragment_length * n_fragments:
@@ -31,14 +29,4 @@ def sample_fasta_files(input_dir, output_dir, fragment_length, n_fragments):
                 f.write(str(fragment) + '\n')
 
         print(f"Successfully generated {len(fragments)} fragments, written to {output_file}")
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Subsample fasta files.')
-    parser.add_argument('--input_dir', type=str, required=True, help='Directory containing input fasta files')
-    parser.add_argument('--output_dir', type=str, required=True, help='Directory to save output fasta files')
-    parser.add_argument('--fragment_length', type=int, default=10000, help='Length of each fragment')
-    parser.add_argument('--n_fragments', type=int, default=10, help='Number of fragments to sample')
-    args = parser.parse_args()
-
-    sample_fasta_files(args.input_dir, args.output_dir, args.fragment_length, args.n_fragments)
 
