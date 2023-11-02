37bc1bcaa3c4bef7c1371e33f839b989791cc960
Philipp Muench <philipp.muench@helmholtz-hzi.de>
2023-11-02 10:31:00 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="genomenet_helper",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'Bio',
+        'numpy',
+    ],
+    entry_points={
+        'console_scripts': [
+            'genomenet_helper=genomenet_helper.__main__:main',
+        ],
+    },
+)
